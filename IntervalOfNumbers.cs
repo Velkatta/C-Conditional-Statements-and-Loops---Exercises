8dc876c68bb75cba191c1e96bbb2e147a1eb43c3
Velkatta <Velkatta@users.noreply.github.com>
2018-11-05 22:05:26 +0200

    Add files via upload

---
+++ IntervalOfNumbers.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _06._Interval_of_Numbers
+{
+    class IntervalOfNumbers
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+            //for (int i = Math.Min(firstNumber, secondNumber); i <= Math.Max(firstNumber, secondNumber); i++)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            int bigger = Math.Max(firstNumber, secondNumber);
+            int smaller = Math.Min(firstNumber, secondNumber);
+            
+            while (smaller <= bigger)
+            {
+                Console.WriteLine(smaller);
+                smaller++;
+            }
+        }
+    }
+}
