d8609066cca3b66063de419e23c777097f5fcf7e
Velkatta <Velkatta@users.noreply.github.com>
2018-11-05 22:05:47 +0200

    Add files via upload

---
+++ CakeIngredients.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _07._Cake_Ingredients
+{
+    class CakeIngredients
+    {
+        static void Main(string[] args)
+        {
+            string ingredient = Console.ReadLine();
+            int count = 0;
+
+            while (ingredient != "Bake!")
+            {
+                Console.WriteLine($"Adding ingredient {ingredient}.");
+                count++;
+                ingredient = Console.ReadLine();
+            }
+            Console.WriteLine($"Preparing cake with {count} ingredients.");
+
+
+        }
+    }
+}
