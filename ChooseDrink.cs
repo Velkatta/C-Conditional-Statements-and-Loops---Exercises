fbcce5dbe1919224822694eb56ea612b7737cb00
Velkatta <Velkatta@users.noreply.github.com>
2018-11-05 22:03:03 +0200

    Add files via upload

---
+++ ChooseDrink.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _01._Choose_a_Drink
+{
+    class ChooseDrink
+    {
+        static void Main(string[] args)
+        {
+            string profession = Console.ReadLine();
+            string drink = null;
+
+            if (profession == "Athlete")
+            {
+                drink = "Water";
+            }
+            else if (profession == "Businessman" || profession ==" businesswoman")
+            {
+                drink = "Coffee";
+            }
+            else if (profession == "SoftUni Student")
+            {
+                drink = "Beer";
+            }
+            else
+            {
+                drink = "Tea";
+            }
+            Console.WriteLine(drink);        
+        }
+    }
+}
